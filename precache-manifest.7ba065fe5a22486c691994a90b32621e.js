be3f691cef7a86ca6adcde648df35d0a3ca870d4
ravibp <ravi.prasanna@infosys.com>
2020-01-25 19:09:11 +0530

    Updates

---
+++ precache-manifest.7ba065fe5a22486c691994a90b32621e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28aee08510ec76a428160a1eb892b7b6",
+    "url": "/employee-app/index.html"
+  },
+  {
+    "revision": "c431636423b84162482b",
+    "url": "/employee-app/static/css/main.5b21bb4d.chunk.css"
+  },
+  {
+    "revision": "0af17e763100bab9fd67",
+    "url": "/employee-app/static/js/2.f58ad8e1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/employee-app/static/js/2.f58ad8e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c431636423b84162482b",
+    "url": "/employee-app/static/js/main.781fd333.chunk.js"
+  },
+  {
+    "revision": "f63791db67983f2d09ab",
+    "url": "/employee-app/static/js/runtime-main.e952b4f0.js"
+  }
+]);
